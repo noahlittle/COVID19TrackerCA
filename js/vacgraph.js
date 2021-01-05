357b2f508b3cc943f9c14baa8fea47a00f4a5751
Evert Timberg <evert.timberg@gmail.com>
2021-01-04 19:05:39 -0500

    Set chart tooltips to 'index' mode to show all traces at once

--- js/vacgraph.js
+++ js/vacgraph.js
@@ -176,6 +176,9 @@ function draw(graphConfig) {
             },
             legend: {
                 display: true
+            },
+            tooltips: {
+                mode: 'index'
             }
         }
     });
