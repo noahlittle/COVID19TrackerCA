80d52adb1bde985d21559970aa9b659cb75c95fa
noahlittle <61997107+noahlittle@users.noreply.github.com>
2021-01-09 09:55:44 -0600

    Daily Ppl Update

--- js/vacmain.js
+++ js/vacmain.js
@@ -34,8 +34,8 @@ $(document).ready(() => {
         // hospitalizationsChange = data.change_hospitalizations;
         
         vaccinations = data.total_vaccinations;
-        peopleVaccinated = data.total_vaccinations - 4588;
-        percentVaccinated = Math.floor((data.total_vaccinations - 4588) / canadaPopulation * 100) / 100;
+        peopleVaccinated = data.total_vaccinations - 7291;
+        percentVaccinated = Math.floor((data.total_vaccinations - 7291) / canadaPopulation * 100) / 100;
         vaccinationsChange = data.change_vaccinations;
         vaccinesDistributed = data.total_vaccines_distributed;
 
